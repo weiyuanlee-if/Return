b3bfc9101925521972cdedb5f68c2eaec2fcb135
weiyuanlee <weiyuanleeif@gmail.com>
2021-03-08 15:51:47 -0500

    add files, coded player choice input, welcome

--- RPSRedo/RPSRedo/Program.cs
+++ RPSRedo/RPSRedo/Program.cs
@@ -6,7 +6,56 @@ namespace RPSRedo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string playerName;
+            string playerChoice;
+
+            Console.WriteLine("Let's play Rock, Paper, Scissors! ;)");
+            Console.WriteLine("What is your name?");
+            string playerNameInput = Console.ReadLine();
+            playerName = playerNameInput;
+            Console.WriteLine("Hello, {0}", playerName);
+            Console.WriteLine("Let's see who goes first.");
+            playerChoice = Console.ReadLine();
+
+            if (playerChoice == "Rock")
+            {
+                Console.WriteLine("You chose: Rock");
+            }
+            else if (playerChoice == "Paper")
+            {
+                Console.WriteLine("You chose: Paper");
+            }
+            else if (playerChoice == "Scissors")
+            {
+                Console.WriteLine("You chose: Scissors");
+            }
+            else
+            {
+                Console.WriteLine("That's not an RPS move, sorry?");
+            }
+
+            
+
+           
+
+
+
+
+
+        }
+        public void ComputerChoice()
+        {
+            Random computerChoice = new Random();
+            int computerChoiceInput;
+            computerChoiceInput = computerChoice.Next(1, 4);
+
+            switch (computerChoice)
+            {
+                case 1:
+                    {
+                        Console.WriteLine("");
+                    }
+            }
         }
     }
 }
