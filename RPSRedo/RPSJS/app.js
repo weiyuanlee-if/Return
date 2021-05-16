e4ab550850170c8ab512ae5e318b372e07a17305
Lixian <weiyuanleeif@gmail.com>
2021-05-16 18:38:48 -0400

    console story

--- RPSRedo/RPSJS/app.js
+++ RPSRedo/RPSJS/app.js
@@ -1,6 +1,23 @@
-var playerName = "";
+const playerName = "";
 var playerScore = 0;
 var compputerScore = 0;
 var levelcounter = 1;
 
+console.log("Rock, Paper Scissors");
+cont playerName = prompt("Hahah. What your name again, son?");
+console.log("I'm getting so forgetful...don't mind if you remind me again later?")
+console.log("Grandpa pats his lap. The fire on the torch cracks.")
+console.log("So, you wanna play Rock, Paper, Scissors?")
+console.log("I lost my other games in the flooding.")
+console.log("I don't got no monopoly or atari or whatever the fuck. Jus these two hands.")
+console.log("He holds up his two hard hands that have been the reciever of many experiences.")
+console.log("As a bare, naked force against prickly, wet skin back during the segregated days of World War II.")
+console.log("A crucial manipulator for the workings of dishes in boiling kitchens.")
+console.log("To touch those hands was to touch what probably felt like a millenium of labor.")
+console.log("You look down and you see they're generational.")
+console.log("Well, first. Let's see who goes first.")
 
+
+
+
+function 
